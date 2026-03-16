bcbcbcb8b42f2c101670c81c51e8e4a53930028b
Jatinder.Singh <Jatinder.Singh@q3tech.com>
2026-03-17 00:11:41 +0530

    Catalog API Implementation

---
+++ src/Services/Catalog/Catalog.Api/GlobalUsing.cs
@@ -0,0 +1,11 @@
+﻿global using Carter;
+
+global using Mapster;
+
+global using MediatR;
+
+global using Marten;
+
+global using BuildingBlocks.CQRS;
+
+global using Catalog.Api.Models;
---
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
+builder.Services.AddMarten(config =>
+{
+    config.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+var app = builder.Build();
+
+app.MapCarter();
+
+app.Run();
